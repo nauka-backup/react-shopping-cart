6bae12e71cfd4a053b666500b51838989f6c36c1
Magdalena Piecuch <magdalena.pie@protonmail.com>
2021-03-08 16:54:42 +0100

    product component implemented

--- src/components/pages/ProductsList/ProductsList.js
+++ src/components/pages/ProductsList/ProductsList.js
@@ -1,21 +1,40 @@
 // feature 1
 import React from 'react';
 import './ProductsList.css';
+import data from '../../../data.json';
+import Products from '../../Products/Products';
 
-function ProductsList() {
-    return (
-        <div className="grid-container">
-            <header className="grid-title">
-                <h1>browse the products</h1>
-            </header>
-            <aside className="filter-container">
-                <h2>filter things</h2>
-                <div>produkt 1</div>
-                <div>produkt 2</div>
-            </aside>
-            <main>produkty</main>
-        </div>
-    );
+class ProductsList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: data.products,
+            size: '',
+            sort: '',
+        };
+    }
+    render() {
+        return (
+            <div className="grid-container">
+                <header className="grid-title">
+                    <h1>browse the products</h1>
+                </header>
+                <aside className="filter-container">
+                    <h2>filter things</h2>
+                    <div>produkt 1</div>
+                    <div>produkt 2</div>
+                </aside>
+                <main>
+                    <div className="content">
+                        <div className="main">
+                            <Products products={this.state.products} />
+                        </div>
+                        <div className="sidebar">cart items</div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default ProductsList;
